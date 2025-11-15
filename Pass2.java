5785eb009b57903d4cbde70e8b21cd8800b831f4
Shubham Asane <aherswami2019@gmail.com>
2025-11-15 07:27:22 +0530

    Update Pass2.java

--- Pass2.java
+++ Pass2.java
@@ -2,11 +2,25 @@ import java.util.*;
 
 class Pass2 {
     public static void main(String[] args) {
-        String IC[] = {"(IS,04) A", "(IS,02) B"};
-        Map<String,String> sym = Map.of("A","205", "B","210");
-
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of IC lines: ");
+        int n = sc.nextInt();
+        sc.nextLine();
+        String IC[] = new String[n];
+        for (int i = 0; i < n; i++) {
+            IC[i] = sc.nextLine();
+        }
+        System.out.print("Enter number of symbols: ");
+        int m = sc.nextInt();
+        sc.nextLine();
+        Map<String, String> sym = new HashMap<>();
+        for (int i = 0; i < m; i++) {
+            String s = sc.next();
+            String addr = sc.next();
+            sym.put(s, addr);
+        }
         for (String line : IC) {
-            String op = line.substring(4,6);
+            String op = line.substring(4, 6);
             String symb = line.split(" ")[1];
             System.out.println(op + " " + sym.get(symb));
         }
